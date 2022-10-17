26e62b9542cb505446689fb3541ab8aae5367cbd
ReeFLk <reeflk8@gmail.com>
2022-10-17 20:05:05 +0200

    95%

--- SImon/test.c
+++ SImon/test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <windows.h>
 
 void allumer(bool *status)
 {
@@ -12,7 +13,61 @@ void eteindre(bool *status)
     *status = false;
 }
 
-void mise_en_page(int *choice, bool status)
+void cafe_select(void)
+{
+    unsigned int choice_cafe = 0;
+    printf("1. Café Allongé\n");
+    printf("2. Café fort\n");
+    printf("Choisir le cafe: ");
+    scanf("%d", &choice_cafe);
+    switch (choice_cafe)
+    {
+    case 1:
+        printf("Café allongé en cours!\n");
+        break;
+    case 2:
+        printf("Café fort en cours!\n");
+        break;
+    }
+    sleep(10);
+}
+
+int maintenance(unsigned int *stocks)
+{   
+    unsigned int choice_maint = 0;
+    printf("\e[1;1H\e[2J");
+    printf("**********************************************\n");
+    printf("*               MAINTENANCE                  *\n");
+    printf("*                                            *\n");
+    printf("* 1. Voir les stocks                         *\n");
+    printf("* 2.Ajouter du café                          *\n");
+    printf("* 3.Voir les ventes                          *\n");
+    printf("* 4.Quitter                                  *\n");
+    printf("**********************************************\n");
+    printf("Choisir le mode: ");
+    scanf("%d",&choice_maint);
+    switch(choice_maint){
+        case 1:
+            printf("Il reste %d doses de café", *stocks);
+            sleep(4);
+            break;
+        case 2:
+            printf("Combien de café voulez vous ajouter: ");
+            unsigned int temp = *stocks;
+            scanf("%d", &*stocks);
+            printf("Vous avez ajouter %d doses\n", *stocks);
+            *stocks += temp;
+            sleep(4);
+            break;
+        case 3:
+            sleep(0.1);
+        case 4:
+            return 0;
+    }
+    maintenance(&*stocks);
+}
+
+int mise_en_page(int *choice, bool status, unsigned int *stocks)
 {
     printf("**********************************************\n");
     printf("* 1.Allumer la cafetière                     *\n");
@@ -42,7 +97,7 @@ void mise_en_page(int *choice, bool status)
         else
         {
             printf("\e[1;1H\e[2J");
-            mise_en_page(&*choice, status);
+            mise_en_page(&*choice, status, &*stocks);
         }
     }
     else
@@ -58,17 +113,23 @@ void mise_en_page(int *choice, bool status)
         case 2:
             eteindre(&status);
             break;
+        case 3:
+            cafe_select();
+        case 4:
+            return 0;
+        case 5:
+            maintenance(&*stocks);
         }
     }
     printf("\e[1;1H\e[2J");
-    mise_en_page(&*choice, status);
+    mise_en_page(&*choice, status, &*stocks);
 };
 
 int main(void)
 {
     unsigned int choice = 0;
+    unsigned int stocks = 8;
     bool status = false;
-    mise_en_page(&choice, status);
-
+    mise_en_page(&choice, status, &stocks);
     return 0;
 }
